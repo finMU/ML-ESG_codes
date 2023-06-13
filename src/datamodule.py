8c63e2bef57aad38ff9962852caf00404e125de9
ExcelsiorCJH <excelsiorcjh@gmail.com>
2023-06-13 23:19:00 +0900

    datamodule commit

--- src/datamodule.py
+++ src/datamodule.py
@@ -5,10 +5,12 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
+from transformers import AutoTokenizer
 
 from collections import Counter
 from typing import List, Tuple, Dict
-from transformers import AutoTokenizer
+
+from .label_dict import LABEL_IDX_DICT, IDX_LABEL_DICT
 
 
 class ESGDataModule:
@@ -18,70 +20,83 @@ class ESGDataModule:
         val_path: str = None,
         test_path: str = None,
         tokenizer_name: str = None,
-        use_title: bool = True,
         use_cls_weight: bool = True,
         batch_size: int = 32,
     ):
         self.train_path = train_path
         self.val_path = val_path
         self.test_path = test_path
-        self.use_title = use_title
         self.use_cls_weight = use_cls_weight
         self.loss_weights = None
         self.batch_size = batch_size
         self.tokenizer_name = tokenizer_name
-        self.label_idx_dict = None
+        self.label_idx_dict = LABEL_IDX_DICT
+        self.idx_label_dict = IDX_LABEL_DICT
 
+        self.tokenizer = AutoTokenizer.from_pretrained(self.tokenizer_name)
         self.setup()
 
     def setup(self):
         # train set
-        # TODO: label_dict 불러와서 고정시켜 놓을것
-        train_texts, train_labels, label_idx_dict = self._load_data(self.train_path, self.use_title)
-        self.label_idx_dict = label_idx_dict
-        self.idx_label_dict = {idx: label for label, idx in self.label_idx_dict.items()}
-
-        self.tokenizer = AutoTokenizer.from_pretrained(self.tokenizer_name)
+        train_dict = self._load_data(self.train_path)
         train_encodings = self.tokenizer(
-            train_texts,
+            train_dict["title"],
             add_special_tokens=True,
+            text_pair=train_dict["content"],
             truncation=True,
             padding=True,
             return_tensors="pt",
         )
-        self.trainset = ESGDataset(train_encodings, train_labels, self.label_idx_dict)
-        # valid set
-        valid_texts, valid_labels, vlabel_idx_dict = self._load_data(self.val_path, self.use_title)
-
-        valid_encodings = self.tokenizer(
-            valid_texts,
-            add_special_tokens=True,
-            truncation=True,
-            padding=True,
-            return_tensors="pt",
-        )
-        self.validset = ESGDataset(valid_encodings, valid_labels, self.vlabel_idx_dict)
+        self.trainset = ESGDataset(train_encodings, train_dict["label"], self.label_idx_dict)
 
         if self.use_cls_weight:
-            target_cnt_dict = Counter([self.label_idx_dict[label] for label in train_labels])
+            target_cnt_dict = Counter([self.label_idx_dict[label] for label in train_dict["label"]])
             target_cnt = [target_cnt_dict[idx] for idx in self.idx_label_dict]
 
             loss_weights = [1 - (x / sum(target_cnt)) for x in target_cnt]
             self.loss_weights = torch.FloatTensor(loss_weights)
 
+        # valid set
+        val_dict = self._load_data(self.val_path)
+        val_encodings = self.tokenizer(
+            val_dict["title"],
+            add_special_tokens=True,
+            text_pair=val_dict["content"],
+            truncation=True,
+            padding=True,
+            return_tensors="pt",
+        )
+        self.valset = ESGDataset(val_encodings, val_dict["label"], self.label_idx_dict)
+
+        # test set
+        self.testset = None
+        if self.test_path:
+            test_dict = self._load_data(self.test_path)
+            test_encodings = self.tokenizer(
+                test_dict["title"],
+                add_special_tokens=True,
+                text_pair=test_dict["content"],
+                truncation=True,
+                padding=True,
+                return_tensors="pt",
+            )
+            self.testset = ESGDataset(test_encodings, test_dict["label"], self.label_idx_dict)
+
     def train_dataloader(self):
         return DataLoader(self.trainset, shuffle=True, batch_size=self.batch_size)
 
     def val_dataloader(self):
-        return DataLoader(self.validset, shuffle=True, batch_size=self.batch_size)
+        return DataLoader(self.valset, shuffle=True, batch_size=self.batch_size)
 
     def test_dataloader(self):
-        pass
+        if self.testset:
+            return DataLoader(self.testset, shuffle=False, batch_size=self.batch_size)
+        return None
 
-    def get_num_labels(self):
+    def get_num_labels(self) -> int:
         return len(self.label_idx_dict)
 
-    def _load_data(self, data_path: str, use_title: bool = True) -> Tuple[List[str], List[str]]:
+    def _load_data(self, data_path: str) -> Dict:
         if "json" in data_path:
             df = pd.read_json(data_path)
         elif "csv" in data_path:
@@ -89,18 +104,11 @@ class ESGDataModule:
         elif "parquet" in data_path:
             df = pd.read_parquet(data_path, engine="pyarrow")
 
-        texts = df["news_content"].tolist()
+        titles = df["news_title"].tolist()
+        contents = df["news_content"].tolist()
         labels = df["ESG_label"].tolist()
 
-        # TODO: title, text 따로 리턴하게 만들것
-        if use_title:
-            titles = df["news_title"].tolist()
-
-            assert len(texts) == len(titles)
-            texts = [f"{title} [SEP] {text}" for title, text in zip(titles, texts)]
-
-        label_idx_dict = {label: idx for idx, label in enumerate(set(labels))}
-        return texts, labels, label_idx_dict
+        return {"title": titles, "content": contents, "label": labels}
 
 
 class ESGDataset(Dataset):
